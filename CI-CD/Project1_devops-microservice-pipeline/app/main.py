9164a122268c09869f628b767d430490c47546c5
nithyan1194 <nithyan1194@gmail.com>
2026-08-21 14:40:29 +0530

    added a new repo devops-starter-project

---
+++ CI-CD/Project1_devops-microservice-pipeline/app/main.py
@@ -0,0 +1,14 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return jsonify({"status": "online", "message": "DevOps CI/CD Pipeline Active!"})
+
+@app.route("/health")
+def health():
+    return jsonify({"status": "healthy"}), 200
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
